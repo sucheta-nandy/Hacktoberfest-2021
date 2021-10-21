5402c883411b978d8e691b0c4651555639e919fb
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-10-22 03:34:12 +0530

    CREATE: mergeSort.py added

---
+++ Python/mergeSort.py
@@ -0,0 +1,35 @@
+def mergeSort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        lefthalf = array[:mid]
+        righthalf = array[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] <= righthalf[j]:
+                array[k] = lefthalf[i]
+                i = i + 1
+            else:
+                array[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            array[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            array[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+
+    return array
+
+print(mergeSort([4, 2, 1, 6, 3]))
