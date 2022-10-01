7d3bb0749accba655c14490ebedb0df7d91af945
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-10-01 10:18:17 +0530

    Update LinearSearch.java

--- Java/LinearSearch.java
+++ Java/LinearSearch.java
@@ -1,30 +1,26 @@
-import java.util.*;
-public class lsearch {
-    static void linear(int a[],int n){
-        int f=0,c=1;
-        for(int i=0;i<n;i++){
-            if(a[i]==n){
-                f=1;
-                break;
-            }
-            c++;
-        }
-        if(f==1)
-            System.out.println("Element found at "+c);
-        else
-            System.out.println("Element not found");
-    }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the no. of elements:");
-        int n=sc.nextInt();
-        int a[]=new int[n];
-        System.out.println("Enter the array elements:");
-        for(int i=0;i<n;i++){
-            a[i]=sc.nextInt();
-        }
-        System.out.println("Enter the number to be searched:");
-        int num=sc.nextInt();
-        linear(a,num);
-    }
+import java.util.Scanner;
+// Other imports go here
+// Do NOT change the class name
+class Main{
+static int rec_linear_search(int arr[], int left, int right, int x) 
+{
+  int result;
+  if (right < left)        // The array is exhausted so return -1
+    return -1;
+  if (arr[left] == x)        // If element found return position
+    return left;
+  // Call the function again with new subarray from next element.
+  result = rec_linear_search(arr, left+1, right, x);     
+  return result;    // return the result to the calling function.
+}
+  public static void main(String[] args)
+  {
+    int loc,x,array[]={10,11,12,13,14,25,26,37,48,29};
+    x=13;    // Searched Element.
+    loc = rec_linear_search(array, 0, 10, x);    // Call the search function
+    if(loc != -1)
+      System.out.print("Element found at location : " + loc);
+    else
+      System.out.print("Element not present in the array.");
+  }
 }
